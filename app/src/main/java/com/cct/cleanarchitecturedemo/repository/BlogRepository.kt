ff7d243712ec20f1311c154bec7165fb8d884a9d
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-02-15 09:03:09 +0200

    define data state class

--- app/src/main/java/com/cct/cleanarchitecturedemo/repository/BlogRepository.kt
+++ app/src/main/java/com/cct/cleanarchitecturedemo/repository/BlogRepository.kt
@@ -1,9 +1,14 @@
 package com.cct.cleanarchitecturedemo.repository
 
+import com.cct.cleanarchitecturedemo.data.cash.BlogDao
+import com.cct.cleanarchitecturedemo.data.network.BlogService
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 
 
-class BlogRepository @Inject constructor() {
+class BlogRepository (val dao:BlogDao,val service:BlogService) {
+
+
+
 }

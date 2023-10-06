76db65ce3371865f47a553e07466d2d1b7c194a7
mokhtar <mokhtar.rnd@gmail.com>
2023-10-06 08:22:39 +0300

    create remote data model

---
+++ app/src/main/java/com/cct/cleanarchitecturedemo/data/remote/model/Blog.kt
@@ -0,0 +1,10 @@
+package com.cct.cleanarchitecturedemo.data.remote.model
+
+
+data class Blog(
+    val id: Int,
+    val title: String,
+    val body: String,
+    val image: String,
+    val category: String
+)

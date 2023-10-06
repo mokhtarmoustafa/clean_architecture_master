3e1d5f8c2794b32f8831fe75e0bee8473abc75a6
mokhtar <mokhtar.rnd@gmail.com>
2023-10-06 08:23:25 +0300

    clean

--- app/src/main/java/com/cct/cleanarchitecturedemo/util/BlogApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.cct.cleanarchitecturedemo.util
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class BlogApplication : Application() {
-}

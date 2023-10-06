975406a79e23c67f49795e2b825c73805d8f22c9
mokhtar <mokhtar.rnd@gmail.com>
2023-10-06 08:17:59 +0300

    first commit

--- app/src/main/java/com/cct/cleanarchitecturedemo/ui/MainActivity.kt
+++ app/src/main/java/com/cct/cleanarchitecturedemo/ui/MainActivity.kt
@@ -4,22 +4,13 @@ package com.cct.cleanarchitecturedemo.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
-import android.widget.Toast
-import androidx.activity.viewModels
-import com.cct.cleanarchitecturedemo.R
 import com.cct.cleanarchitecturedemo.databinding.ActivityMainBinding
-import com.cct.cleanarchitecturedemo.model.Blog
-import com.cct.cleanarchitecturedemo.util.ConnectionLiveData
-import com.cct.cleanarchitecturedemo.util.DataState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     //region variables
-    private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
-    private lateinit var connectivityLiveData: ConnectionLiveData
     //endregion
 
     //region events
@@ -36,53 +27,10 @@ class MainActivity : AppCompatActivity() {
 
     //region helper functions
     private fun init() {
-        ConnectionLiveData(this).also { connectivityLiveData = it }
-        connectivityLiveData.observe(this, { isConnected ->
-            if (isConnected) {
-                viewModel.setState(ViewModelState.getBlogs)
-                getBlogs()
-            } else {
-                Toast.makeText(this, getString(R.string.no_internet), Toast.LENGTH_SHORT).show()
 
-            }
-        })
     }
 
-    private fun getBlogs() {
-        viewModel.blogs.observe(this, { dataState ->
-            when (dataState) {
-                is DataState.Success<List<Blog>> -> {
-                    showProgress(false)
-                    showData(dataState.data)
-                }
-                is DataState.Error -> {
-                    showProgress(false)
-                    showErrorMessage(dataState.exceptionMessage)
-                }
-                is DataState.Loading -> {
-                    showProgress(true)
-                }
-            }
-        })
-    }
 
-    private fun showData(data: List<Blog>) {
-        val builder = StringBuilder()
-        data.let { blogs ->
-            if (blogs.isNotEmpty()) {
-                blogs.forEach { builder.appendLine(it.title) }
-            }
-            binding.tvData.text = builder.toString()
-        }
-    }
-
-    private fun showErrorMessage(message: String) {
-        binding.tvData.text = message
-    }
-
-    private fun showProgress(show: Boolean) {
-        binding.progress.visibility = if (show) View.VISIBLE else View.INVISIBLE
-    }
     //endregion
 
     companion object {
--- app/src/main/java/com/cct/cleanarchitecturedemo/ui/ViewModelState.kt
+++
@@ -1,5 +0,0 @@
-package com.cct.cleanarchitecturedemo.ui
-
-sealed class ViewModelState {
-    object getBlogs : ViewModelState()
-}
